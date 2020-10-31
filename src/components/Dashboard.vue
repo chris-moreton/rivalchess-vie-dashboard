9b4044931aea25f957b245c22300a0588d272e58
Chris Moreton <chris@netsensia.com>
2020-10-31 22:47:37 +0000

    Add table headers

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -5,12 +5,18 @@
       <td>
         <h1>Match Ups</h1>
         <table border="1">
-        <AsyncMatchUp v-for="matchUp in data.matchUps" :matchUp="matchUp" />
+          <tr><th>Engine 1</th><th>Engine 2</th><th>Wins</th><th>Losses</th><th>Draws</th><th>Total</th><th>As White</th></tr>
+        <AsyncMatchUp v-for="matchUp in data.matchUpsConsolidated" :matchUp="matchUp" />
         </table>
       </td>
       <td valign="top">
         <h1>Rankings</h1>
         <table border="1">
+          <tr>
+            <th>Engine</th>
+            <th>ELO</th>
+            <th>Played</th>
+          </tr>
           <AsyncRanking v-for="ranking in data.rankings" :ranking="ranking" />
         </table>
       </td>
--- src/components/MatchUp.vue
+++ src/components/MatchUp.vue
@@ -1,5 +1,13 @@
 <template>
-  <tr border="1"><td>{{ matchUp.engine1 }}</td><td>{{ matchUp.engine2 }}</td><td>{{ matchUp.result }}</td><td>{{ matchUp.cnt }}</td></tr>
+  <tr border="1">
+    <td>{{ matchUp.engine1 }}</td>
+    <td>{{ matchUp.engine2 }}</td>
+    <td>{{ matchUp.engine1Wins }}</td>
+    <td>{{ matchUp.engine2Wins }}</td>
+    <td>{{ matchUp.draws }}</td>
+    <td>{{ matchUp.draws + matchUp.engine1Wins + matchUp.engine2Wins }}</td>
+    <td>{{ matchUp.engine1AsWhiteCount }} {{ ((matchUp.engine1AsWhiteCount / (matchUp.draws + matchUp.engine1Wins + matchUp.engine2Wins)) * 100) | a }}%</td>
+  </tr>
 </template>
 
 <script>
