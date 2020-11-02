4aa3dd72cca538c1c7b82c6523dff679962b8ff0
Chris Moreton <chris@netsensia.com>
2020-11-02 10:32:16 +0000

    Set interval to 10 seconds

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -55,7 +55,7 @@ export default {
   async setup() {
     const { data, error, load, totalMatchesPlayed } = getStatistics();
     await load();
-    setInterval(load, 10000)
+    setInterval(await load, 10000)
 
     return { data, error, totalMatchesPlayed };
   },
